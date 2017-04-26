19ee13391f43f124f7338e6c320091529d91ecc5
Scott Ranger <millenniumnight@googlemail.com>
2017-04-26 12:40:28 +0100

    Update conditions.elm

--- scripts/conditions.elm
+++ scripts/conditions.elm
@@ -2,13 +2,10 @@ import Html
 
 main =
   isEven 10
-  |> toString
-    |>
-      Html.text
-
--- Function signature
+    |> toString
+    |> Html.text
+  
 isEven: Int -> Bool
--- Function implementation
 isEven num =
   if rem num 2 == 0 then
     True
