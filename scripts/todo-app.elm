0677c64559fe7459b7d76debff137676e018c29d
Scott Ranger <millenniumnight@googlemail.com>
2017-04-28 15:13:46 +0100

    Started todo-app from fresh

--- scripts/todo-app.elm
+++ scripts/todo-app.elm
@@ -1,114 +1,91 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Date exposing (..)
-import Date.Format exposing (format)
-import Task
+import Date exposing (Date)
+import List
 
+main: Program Never Model Message
 main =
-  program { init = init, view = view, update = update, subscriptions = subscriptions }
-
-type alias Model =
-  { nextId: Int
-  , textValue: String
-  , todos: List Todo
-  }
+  program { init = init, update = update, view = view, subscriptions = subscriptions }
 
 type alias Todo =
   { id: Int
   , description: String
-  , completed: Bool
-  , date: Maybe Date
+  , isCompleted: Bool
+  , createdAt: Maybe Date
   }
 
-init =
-  let
-    model =
-      { nextId = 0
-      , textValue = ""
-      , todos = []
-      }
-  in
-    (model, Cmd.none)
+type alias Model =
+  { nextId: Int
+  , todos: List Todo
+  , inputValue: String
+  }
 
-type Msg
-  = AddTodo
-  | TextValue String
-  | ToggleCompleted Int
+type Message
+  = CreateTodo
   | DeleteTodo Int
-  | AddDateToTodo Int Date
+  | ToggleCompletedTodo Int
+  | OnInputValue String
 
-view : Model -> Html Msg
+init: (Model, Cmd Message)
+init =
+  ((Model 0 [] ""), Cmd.none)
+
+view: Model -> Html Message
 view model =
   div []
-    [ h1 [] [ text "Todos" ]
-    , div []
-      [ input [ type_ "text", onInput TextValue, value model.textValue ] []
-      , button [ onClick AddTodo ] [ text "Add Todo" ]
-      ]
-    , List.map toTodo model.todos |> div []
-    , div [] []
+  [ h1 [] [ text "Todo App" ]
+  , div []
+    [ input [ type_ "text", value model.inputValue, onInput OnInputValue ] []
+    , button [ onClick CreateTodo ] [ text "Add Todo" ]
+    ]
+  , div [] [ todoList model ]
+  ]
+
+todoList: Model -> Html Message
+todoList model =
+  List.map mapTodoToLi model.todos |> ul []
+
+todoStyles: Todo -> Attribute Message
+todoStyles todo =
+  case todo.isCompleted of
+    True ->
+      style [ ("text-decoration", "line-through") ]
+    False ->
+      style []
+
+mapTodoToLi: Todo -> Html Message
+mapTodoToLi todo =
+  li []
+    [ div [ todoStyles todo, onClick <| ToggleCompletedTodo todo.id ] [ text todo.description ]
+    , button [ onClick <| DeleteTodo todo.id ] [ text "Delete" ]
     ]
 
-update: Msg -> Model -> (Model, Cmd Msg)
+newTodo: Model -> Todo
+newTodo model =
+  Todo model.nextId model.inputValue False Nothing
+
+update: Message -> Model -> (Model, Cmd Message)
 update msg model =
   case msg of
-    AddTodo ->
+    OnInputValue value ->
+      ({ model | inputValue = value }, Cmd.none)
+    CreateTodo ->
       let
-        currentId = model.nextId
+        todos = newTodo model :: model.todos
       in
-        ({ model
-          | todos =  model.todos ++ [ (Todo currentId model.textValue False Nothing) ]
-          , textValue = ""
-          , nextId = model.nextId + 1
-        }, Task.perform (AddDateToTodo currentId) Date.now)
-
-    TextValue value ->
-      ({ model | textValue = value }, Cmd.none)
-
-    ToggleCompleted id ->
-      let
-        toggleCompleted todo =
-          if todo.id == id then
-            { todo | completed = not todo.completed }
-          else
-            todo
-      in
-        ({ model | todos = List.map toggleCompleted model.todos }, Cmd.none)
+      ({ model | inputValue = "", todos = todos, nextId = model.nextId + 1 }, Cmd.none)
     DeleteTodo id ->
-      ({ model | todos = List.filter (\t -> t.id /= id) model.todos }, Cmd.none)
-    AddDateToTodo id date ->
       let
-        changeDate todo =
-          if todo.id == id then
-            { todo | date = Just date }
-          else
-           todo
+        todos = List.filter (\t -> t.id /= id) model.todos
       in
-        ({ model | todos = List.map changeDate model.todos }, Cmd.none)
+        ({ model | todos = todos }, Cmd.none)
+    ToggleCompletedTodo id ->
+      let
+        todos = List.map (\t -> if t.id == id then { t | isCompleted = not t.isCompleted } else t ) model.todos
+      in
+        ({ model | todos = todos }, Cmd.none)
 
-toTodo: Todo -> Html Msg
-toTodo value =
-  if value.completed == True then
-    div []
-    [ p [ onClick (ToggleCompleted value.id), style [("text-decoration", "line-through")] ] [text value.description]
-    , button [ onClick (DeleteTodo value.id) ] [ text "Delete" ]
-    , div [] [ text <| createdAt value.date ]
-    ]
-  else
-    div []
-    [ p [ onClick (ToggleCompleted value.id) ] [text value.description]
-    , button [ onClick (DeleteTodo value.id) ] [ text "Delete" ]
-    , div [] [ text <| createdAt value.date ]
-    ]
-
-createdAt: Maybe Date -> String
-createdAt date =
-  case date of
-    Nothing ->
-      ""
-    Just date ->
-      format "Created at: %a %e %b %Y - %k:%M%p" date
-
+subscriptions: Model -> Sub Message
 subscriptions model =
   Sub.none
